b8c57b33283fca4463b562ced6c8ec78059e4257
Adarsh <22je0040@iitism.ac.in>
2025-03-25 03:54:18 +0530

    Share Button added to copy the URL in the clipboard

--- Frontend/src/components/atoms/ShareButton.jsx
+++ Frontend/src/components/atoms/ShareButton.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import { Button } from '../ui/button';
-import { Share2 } from 'lucide-react';
+"use client";
+import React from "react";
 
-function ShareButton() {
-  return (
-    <>
-      <Button><Share2/> Share</Button>
-    </>
-  );
-}
+import { Share2Icon } from "lucide-react";
+import { Button } from "../ui/button";
 
-export default ShareButton
+const ShareButton = () => {
+    const handleShare = () => {
+        // url
+        const url = window.location.href;
+        // clipboard -> api browser -> promise 
+        navigator.clipboard
+            .writeText(url)
+            .then(() => {
+                alert("URL copied to the clipboard");
+            })
+            .catch((err) => {
+                console.error("Failed to copy: ", err);
+                alert("failed to copy url");
+            });
+    };
+    return (
+        <Button onClick={handleShare} >
+            <Share2Icon className="w-4 h-4 mr-2" />
+            Share
+        </Button>
+    );
+};
+
+export default ShareButton;
